45428a11348c9cd66b21c9846275eaa35ebc521f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-07-21 00:59:49 +0000

    hello world text widget

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -169,6 +169,23 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                     );
                   },
                 ),
+                Text(
+                  'Hello World',
+                  style: FlutterFlowTheme.of(context).bodyMedium.override(
+                        font: GoogleFonts.inter(
+                          fontWeight: FlutterFlowTheme.of(context)
+                              .bodyMedium
+                              .fontWeight,
+                          fontStyle:
+                              FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                        ),
+                        letterSpacing: 0.0,
+                        fontWeight:
+                            FlutterFlowTheme.of(context).bodyMedium.fontWeight,
+                        fontStyle:
+                            FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                      ),
+                ),
               ],
             ),
           ),
